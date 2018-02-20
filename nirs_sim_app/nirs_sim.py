5318fca367fc93d5a2871323d0ad5b7b2c56bc18
Jacob Tatz <jacob.tatz@childrens.harvard.edu>
2018-02-20 22:08:09 +0000

    DCS analysis and numba speedup

--- nirs_sim_app/nirs_sim.py
+++ nirs_sim_app/nirs_sim.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numba as nb
 from pymcx import MCX
 
 def create_prop(spec, wavelen):
@@ -8,13 +9,30 @@ def create_prop(spec, wavelen):
     g = spec.get('g', 0.9)
     ind = np.where(spec['waves'] == wavelen)[0][0]
     extinction_coeffs = spec['lut'][ind] / 10
-    media = np.empty((len(concentrations) + 1, 4), np.float64)
+    media = np.empty((len(concentrations) + 1, 4), np.float32)
     media[0] = [0, 0, 1, spec.get('n_external', 1)]
     media[1:, 0] = concentrations @ extinction_coeffs  # mua
     media[1:, 1] = (a * wavelen ** (-b)) / (1-g)  # mus
     media[1:, 2] = g
     media[1:, 3] = spec.get('n', 1.37)
     return media
+
+
+@nb.jit(nopython=True, nogil=True, parallel=False)
+def analysis(detp, prop, tof_domain, tau, k, BFi, ndet, ntof, nmedia, photon_counts, paths, phiTD, g1_top):
+    c = 2.998e+11 # speed of light in mm / s
+    detBins = detp[0].astype(np.intc) - 1
+    tofBins = np.digitize(prop[1:, 3] @ detp[2:(2+nmedia)] / c, tof_domain) - 1
+    tofBins[tofBins == ntof] -= 1
+    pathV = detp[2:(2+nmedia)].T
+    phiV = np.exp(-prop[1:, 0] @ detp[2:(2+nmedia)])
+    prep = (-2*k**2*BFi).astype(np.float32) @ detp[(2+nmedia):(2+2*nmedia)]
+    for i in range(len(detBins)):
+        photon_counts[detBins[i], tofBins[i]] += 1
+        paths[detBins[i], tofBins[i]] += pathV[i]
+        phiTD[detBins[i], tofBins[i]] += phiV[i]
+        for j in range(len(tau)):
+            g1_top[detBins[i], j] += np.exp(prep[i] * tau[j] + path[i])
 
 
 def simulate(spec, wavelength):
@@ -24,10 +42,14 @@ def simulate(spec, wavelength):
     seeds = np.asarray(spec.get('seeds', np.random.randint(0xFFFF, size=run_count)))
     tof_domain = sepc.get('tof_domain', np.append(np.arange(cfg.tstart, cfg.tend, cfg.tstep), cfg.tend))
     c = 2.998e+11 # speed of light in mm / s
+    k = (2*np.pi*cfg.prop[1:, 3]/(wavelength*10**-6))
+    tau = spec.get('tau', np.logspace(-8, -2))
+    BFi = spec.get('BFi', 3e-6)
     ndet, ntof, nmedia = len(cfg.detpos), len(tof_domain) - 1, len(cfg.prop) - 1
     phiTD = np.zeros((ndet, ntof), np.float64)
     paths = np.zeros((ndet, ntof, nmedia), np.float64)
-    photon_counts = 0
+    photon_counts = np.zeros((ndet, ntof), np.int64)
+    g1_top = np.zeros((ndet, len(tau)), np.float64)
     fslice = 0
     for seed in seeds:
         cfg.seed = int(seed)
@@ -35,12 +57,7 @@ def simulate(spec, wavelength):
         detp = result["detphoton"]
         if detp.shape[1] >= cfg.maxdetphoton:
             raise Exception("Too many photons detected: {}".format(detp.shape[1]))
-        detBins = detp[0].astype(np.intc) - 1
-        tofBins = np.digitize(cfg.prop[1:, 3] @ detp[2:(2+nmedia)] / c, tof_domain) - 1
-        tofBins[tofBins == ntof] -= 1
-        np.add.at(phiTD, (detBins, tofBins), np.exp(-cfg.prop[1:, 0] @ detp[2:(2+nmedia)]))
-        photon_counts += np.bincount(detBins*ntof+tofBins, minlength=ndet*ntof).reshape((ndet, ntof))
-        np.add.at(paths, (detBins, tofBins), detp[2:(2+nmedia)].T)
+        analysis(detp, cfg.prop, tof_domain, tau, k, BFi, ndet, ntof, nmedia, photon_counts, paths, phiTD, g1_top)
         fslice += result["fluence"][spec['slice']]
         del tofBins
         del detBins
