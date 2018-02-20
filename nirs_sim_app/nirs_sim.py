d86585f51e98b31a774cd52873a2f278532d64d1
Jacob Tatz <jacob.tatz@childrens.harvard.edu>
2018-02-20 17:21:23 +0000

    Added pathlength histogram and better names

--- nirs_sim_app/nirs_sim.py
+++ nirs_sim_app/nirs_sim.py
@@ -22,27 +22,30 @@ def simulate(spec, wavelength):
     cfg.prop = create_prop(spec, wavelength)
     run_count = spec.get('run_count', 1)
     seeds = np.asarray(spec.get('seeds', np.random.randint(0xFFFF, size=run_count)))
-    tof_domain = np.append(np.arange(cfg.tstart, cfg.tend, cfg.tstep), cfg.tend)
+    tof_domain = sepc.get('tof_domain', np.append(np.arange(cfg.tstart, cfg.tend, cfg.tstep), cfg.tend))
     c = 2.998e+11 # speed of light in mm / s
-    n1, n2 = len(cfg.detpos), len(tof_domain) - 1
-    phiTD = np.zeros((n1, n2), np.float64)
+    ndet, ntof, nmedia = len(cfg.detpos), len(tof_domain) - 1, len(cfg.prop) - 1
+    phiTD = np.zeros((ndet, ntof), np.float64)
+    paths = np.zeros((ndet, ntof, nmedia), np.float64)
     photon_counts = 0
     fslice = 0
     for seed in seeds:
         cfg.seed = int(seed)
         result = cfg.run(2)
-        detp = result["exportdetected"]
+        detp = result["detphoton"]
         if detp.shape[1] >= cfg.maxdetphoton:
             raise Exception("Too many photons detected: {}".format(detp.shape[1]))
         detBins = detp[0].astype(np.intc) - 1
-        tofBins = np.digitize(cfg.prop[1:, 3] @ detp[2:(len(cfg.prop) + 1)] / c, tof_domain) - 1
-        tofBins[tofBins == n2] -= 1
-        np.add.at(phiTD, (detBins, tofBins), np.exp(-cfg.prop[1:, 0] @ detp[2:(len(cfg.prop) + 1)]))
-        photon_counts += np.bincount(detBins*n2+tofBins, minlength=n1*n2).reshape((n1, n2))
-        fslice += result["exportfield"][spec['slice']]
+        tofBins = np.digitize(cfg.prop[1:, 3] @ detp[2:(2+nmedia)] / c, tof_domain) - 1
+        tofBins[tofBins == ntof] -= 1
+        np.add.at(phiTD, (detBins, tofBins), np.exp(-cfg.prop[1:, 0] @ detp[2:(2+nmedia)]))
+        photon_counts += np.bincount(detBins*ntof+tofBins, minlength=ndet*ntof).reshape((ndet, ntof))
+        np.add.at(paths, (detBins, tofBins), detp[2:(2+nmedia)].T)
+        fslice += result["fluence"][spec['slice']]
         del tofBins
         del detBins
         del detp
         del result
     fslice /= run_count
-    return {'Photons': photon_counts, 'Phi': phiTD, 'Seeds': seeds, 'Slice': fslice}
+    paths /= photon_counts[:, :, np.newaxis]
+    return {'Photons': photon_counts, 'Paths': paths, 'Phi': phiTD, 'Seeds': seeds, 'Slice': fslice}
