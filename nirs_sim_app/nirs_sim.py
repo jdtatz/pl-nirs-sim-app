6d911faf8370979b48f6f58baf8d8686831f10fb
Jacob Tatz <jacob.tatz@childrens.harvard.edu>
2018-02-21 18:11:51 +0000

    Added frequncy domain analysis for now

--- nirs_sim_app/nirs_sim.py
+++ nirs_sim_app/nirs_sim.py
@@ -19,19 +19,22 @@ def create_prop(spec, wavelen):
 
 
 @nb.jit(nopython=True, nogil=True, parallel=False)
-def analysis(detp, prop, tof_domain, tau, wavelength, BFi, ndet, ntof, nmedia, pcounts, paths, phiTD, g1_top):
+def analysis(detp, prop, tof_domain, tau, wavelength, BFi, freq, ndet, ntof, nmedia, pcounts, paths, phiTD, phiFD, g1_top):
     c = 2.998e+11 # speed of light in mm / s
     detBins = detp[0].astype(np.intc) - 1
     tofBins = np.digitize(prop[1:, 3] @ detp[2:(2+nmedia)], c * tof_domain) - 1
     tofBins[tofBins == ntof] -= 1
     path = -prop[1:, 0] @ detp[2:(2+nmedia)]
     phis = np.exp(path)
+    fwave = (-prop[1:, 0] + 2j * np.pi * freq * prop[1:, 3] / c).astype(np.complex64)
+    fds = np.exp(fwave @ detp[2:(2+nmedia)].astype(np.complex64))
     k = (2*np.pi*prop[1:, 3]/(wavelength*1e-6))
     prep = (-2*k**2*BFi).astype(np.float32) @ detp[(2+nmedia):(2+2*nmedia)]
     for i in range(len(detBins)):
         pcounts[detBins[i], tofBins[i]] += 1
         paths[detBins[i], tofBins[i]] += detp[2:(2+nmedia), i]
         phiTD[detBins[i], tofBins[i]] += phis[i]
+        phiFD[detBins[i]] += fds[i]
         for j in range(len(tau)):
             g1_top[detBins[i], j] += np.exp(prep[i] * tau[j] + path[i])
 
@@ -44,8 +47,10 @@ def simulate(spec, wavelength):
     tof_domain = spec.get('tof_domain', np.append(np.arange(cfg.tstart, cfg.tend, cfg.tstep), cfg.tend))
     tau = spec.get('tau', np.logspace(-8, -2))
     BFi = spec['BFi']
+    freq = spec.get('frequency', 110e6)
     ndet, ntof, nmedia = len(cfg.detpos), len(tof_domain) - 1, len(cfg.prop) - 1
     phiTD = np.zeros((ndet, ntof), np.float64)
+    phiFD = np.zeros(ndet, np.complex128)
     paths = np.zeros((ndet, ntof, nmedia), np.float64)
     pcounts = np.zeros((ndet, ntof), np.int64)
     g1_top = np.zeros((ndet, len(tau)), np.float64)
@@ -56,11 +61,11 @@ def simulate(spec, wavelength):
         detp = result["detphoton"]
         if detp.shape[1] >= cfg.maxdetphoton:
             raise Exception("Too many photons detected: {}".format(detp.shape[1]))
-        analysis(detp, cfg.prop, tof_domain, tau, wavelength, BFi, ndet, ntof, nmedia, pcounts, paths, phiTD, g1_top)
+        analysis(detp, cfg.prop, tof_domain, tau, wavelength, BFi, freq, ndet, ntof, nmedia, pcounts, paths, phiTD, phiFD, g1_top)
         fslice += result["fluence"][spec['slice']]
         del detp
         del result
     fslice /= run_count
     paths /= pcounts[:, :, np.newaxis]
-    g1 = g1_top / np.sum(phiTD, axis=1)[:, np.newaxis] 
-    return {'Photons': pcounts, 'Paths': paths, 'Phi': phiTD, 'Seeds': seeds, 'Slice': fslice, 'g1': g1}
+    g1 = g1_top / np.sum(phiTD, axis=1)[:, np.newaxis]
+    return {'Photons': pcounts, 'Paths': paths, 'PhiTD': phiTD, 'PhiFD': phiFD, 'Seeds': seeds, 'Slice': fslice, 'g1': g1}
