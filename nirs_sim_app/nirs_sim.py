a39709dcab32f1cdd6bae96c658ea138e098487a
Jacob Tatz <jacob.tatz@childrens.harvard.edu>
2018-03-05 22:33:50 +0000

    Less error prone way of computing layer properties

--- nirs_sim_app/nirs_sim.py
+++ nirs_sim_app/nirs_sim.py
@@ -2,20 +2,20 @@ import numpy as np
 import numba as nb
 from pymcx import MCX
 
-def create_prop(spec, wavelen):
-    concentrations = spec['concentrations']
-    a = spec['scatter_a']
-    b = spec['scatter_b']
-    g = spec.get('g', 0.9)
-    ind = np.where(spec['waves'] == wavelen)[0][0]
-    extinction_coeffs = spec['lut'][ind] / 10
-    media = np.empty((len(concentrations) + 1, 4), np.float32)
-    media[0] = [0, 0, 1, spec.get('n_external', 1)]
-    media[1:, 0] = concentrations @ extinction_coeffs  # mua
-    media[1:, 1] = (a * wavelen ** (-b)) / (1-g)  # mus
-    media[1:, 2] = g
-    media[1:, 3] = spec.get('n', 1.37)
-    return media
+
+def create_props(spec, wavelen):
+    layers = spec['layers']
+    lprops = spec['layer_properties']
+    ext_coeff = {k: np.interp(wavelen, *itr) for k, itr in spec['extinction_coeffs'].items()}
+    media = np.empty((1+len(layers), 4), np.float32)
+    media[0] = 0, 0, 1, spec.get('n_external', 1)
+    for i, l in enumerate(layers):
+        lp = lprops[l]
+        g = lp['g']
+        mua = sum(ext_coeff[k] * lp['components'][k] for k in ext_coeff)
+        mus = lp['Scatter A'] * wavelen ** -lp['Scatter b'] / (1 - g)
+        media[1+i] = mua, mus, g, lp['n']
+    return media, np.stack(lprops[l]['BFi'] for l in layers)
 
 
 @nb.jit(nopython=True, nogil=True, parallel=False)
@@ -42,12 +42,11 @@ def analysis(detp, prop, tof_domain, tau, wavelength, BFi, freq, ndet, ntof, nme
 def simulate(spec, wavelength):
     cfg = spec['mcx']
     cfg.ismomentum = True
-    cfg.prop = create_prop(spec, wavelength)
+    cfg.prop, BFi = create_props(spec, wavelength)
     run_count = spec.get('run_count', 1)
     seeds = np.asarray(spec.get('seeds', np.random.randint(0xFFFF, size=run_count)))
     tof_domain = spec.get('tof_domain', np.append(np.arange(cfg.tstart, cfg.tend, cfg.tstep), cfg.tend))
     tau = spec.get('tau', np.logspace(-8, -2))
-    BFi = spec['BFi']
     freq = spec.get('frequency', 110e6)
     ndet, ntof, nmedia = len(cfg.detpos), len(tof_domain) - 1, len(cfg.prop) - 1
     phiTD = np.zeros((ndet, ntof), np.float64)
